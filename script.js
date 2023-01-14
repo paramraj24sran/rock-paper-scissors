2fbc0cea93f3f6d975168b1f80a7a4239b52f94e
paramraj24sran <singh7856paramraj@gmail.com>
2023-01-13 19:00:56 -0500

    final

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 console.log("yo");
 
-function getComputerChoice(){
-    const choices = ["rock","paper","scissors"];
-    const choice = choices[Math.floor(Math.random()*choices.length)];
-    console.log(choice);
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const choice = choices[Math.floor(Math.random() * choices.length)];
+  console.log(choice);
 }
 
-getComputerChoice();
+function checkWinner(userChoice, computerChoice) {
+  if (userChoice == computerChoice) {
+    return "Tie";
+  } else if (
+    (userChoice == "rock" && computerChoice == "scissors") ||
+    (userChoice == "paper" && computerChoice == "rock") ||
+    (userChoice == "scissors" && computerChoice == "paper")
+  ) {
+    return "User";
+  } else{
+    return "Computer";
+  }
+}
+
+function playRound(userChoice, computerChoice) {
+  const result = checkWinner(userChoice, computerChoice);
+
+  if (result == "Tie") {
+    return "Its a tie";
+  } else if (result == "User") {
+    return `You won ${userChoice} beats ${computerChoice}`;
+  } else return `You lost ${computerChoice} beats ${userChoice}`;
+}
+
+const userChoice = "rock";
+const computerChoice = getComputerChoice();
+console.log(playRound(userChoice, computerChoice));
